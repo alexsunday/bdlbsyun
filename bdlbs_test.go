0e9d07c6896143b37674866968f885da8e5a1fac
espush <webmaster@espush.cn>
2019-03-12 11:56:51 +0800

    add test

---
+++ bdlbs_test.go
@@ -0,0 +1,36 @@
+package bdlbsyun
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_geoconv(t *testing.T) {
+	api := NewBaiduLbsApi("0T3dbQsKaAxfsZfskEDm0OvMVANgTqmf")
+	lng, lat, err := api.GeoConvert(113.3532180, 23.1407146, GEOCONV_F_WGS84, GEOCONV_F_BD_LL)
+	if err != nil {
+		return
+	}
+	fmt.Println(lng, lat)
+}
+
+func Test_loc2addr(t *testing.T) {
+	api := NewBaiduLbsApi("0T3dbQsKaAxfsZfskEDm0OvMVANgTqmf")
+	result, err := api.Loc2Addr(113.36522341182265, 23.143990588355013)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func Test_addr2loc(t *testing.T) {
+	api := NewBaiduLbsApi("0T3dbQsKaAxfsZfskEDm0OvMVANgTqmf")
+	lng, lat, err := api.Addr2Loc("", "广东省广州市白云区同和街道石握路56号")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(lng, lat)
+}
