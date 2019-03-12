f00bf32f03a869d6ea4854545b4e3abe7ccfcb28
espush <webmaster@espush.cn>
2019-03-12 11:51:35 +0800

    add wrappered func.

--- caller.go
+++ caller.go
@@ -1,14 +1,15 @@
 package bdlbsyun
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const (
-	API_SERVER = "api.map.baidu.com"
+	API_SERVER       = "api.map.baidu.com"
 	GEOCONV_F_WGS84  = "1"
 	GEOCONV_F_SOGOU  = "2"
 	GEOCONV_F_GOOGLE = "3"
@@ -24,21 +25,25 @@ const (
 	GEOCONV_T_BD09MC = "6"
 )
 
-type BaiduApi struct {
+type BaiduLbsApi struct {
 	Ak string
-	Version string
 }
 
+func NewBaiduLbsApi(ak string) *BaiduLbsApi {
+	return &BaiduLbsApi{
+		Ak: ak,
+	}
+}
 
-func (api BaiduApi) call (serviceName string, subService string, params map[string]string, template interface{}) (error){
+func (api *BaiduLbsApi) call(serviceName, subService, version string, params map[string]string, template interface{}) error {
 	params["ak"] = api.Ak
 	temp := "?"
 	for key, value := range params {
 		temp += key + "=" + value + "&"
 	}
-	queryString := temp[0:len(temp) - 1]
-	url := fmt.Sprintf("http://%s/%s/%s/%s%s", 
-		API_SERVER, serviceName, api.Version, 
+	queryString := temp[0 : len(temp)-1]
+	url := fmt.Sprintf("http://%s/%s/%s/%s%s",
+		API_SERVER, serviceName, version,
 		subService, queryString)
 	response, err := http.Get(url)
 	defer response.Body.Close()
@@ -46,31 +51,113 @@ func (api BaiduApi) call (serviceName string, subService string, params map[stri
 		return err
 	}
 	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
 	// fmt.Println(string(body))
 	// 解码过程，并检测相关可能存在的错误
 	if err := json.Unmarshal(body, &template); err != nil {
-		return err	
+		return err
 	}
 	return nil
+}
+
+func (api *BaiduLbsApi) GeoConvert(lng, lat float64, from, to string) (rlng, rlat float64, err error) {
+	params := make(map[string]string)
+	params["coords"] = fmt.Sprintf("%v,%v", lng, lat)
+	params["from"] = from
+	params["to"] = to
+
+	result, err := api.Geoconv(params)
+	if err != nil {
+		return
+	}
+
+	if result.Status != 0 {
+		err = errors.New("baidu lbs cloud response error, status not zero.")
+		return
+	}
+
+	if len(result.Result) != 1 {
+		err = errors.New("baidu lbs cloud response error, content empty.")
+	}
+
+	rlng = result.Result[0].X
+	rlat = result.Result[0].Y
+	return
 }
 
 // 百度地图坐标转换API是一套以HTTP形式提供的坐标转换接口，用于将常用的非百度坐标 (目前支持GPS设备
 // 获取的坐标、 google地图坐标、soso地图坐标、amap地图坐标、mapbar地图坐标）转换成百度地图中使用
 // 的坐标，并可将转化后的坐标在百度地图JavaScript API、车联网API、静态图API、web服务API等产品中使用。
 // http://lbsyun.baidu.com/index.php?title=webapi/guide/changeposition
-func (api BaiduApi) Geoconv (params map[string]string) (*GeoconvRet, error){
+func (api *BaiduLbsApi) Geoconv(params map[string]string) (*GeoconvRet, error) {
 	ret := &GeoconvRet{}
-	err := api.call("geoconv", "", params, ret)
+	err := api.call("geoconv", "", "v1", params, ret)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
+
+func (api *BaiduLbsApi) Addr2Loc(city, addr string) (lng, lat float64, err error) {
+	params := make(map[string]string)
+	params["address"] = addr
+	params["city"] = city
+	params["output"] = "json"
+
+	res, err := api.Geocoding(params)
+	if err != nil {
+		return
+	}
+
+	result, ok := res.(*GeoEncoderRet)
+	if !ok {
+		err = errors.New("api return nil pointer.")
+		return
+	}
+
+	if result.Status != 0 {
+		err = errors.New("baidu lbs cloud response error, status not zero.")
+		return
+	}
+
+	lng = result.Result.Location.Lng
+	lat = result.Result.Location.Lat
+	return
+}
+
+func (api *BaiduLbsApi) Loc2Addr(lng, lat float64) (result *GeocoderRet, err error) {
+	params := make(map[string]string)
+	params["coordtype"] = "bd09ll"
+	params["location"] = fmt.Sprintf("%v,%v", lat, lng)
+	params["output"] = "json"
+	params["latest_admin"] = "1"
+
+	res, err := api.Geocoding(params)
+	if err != nil {
+		return
+	}
+
+	result, ok := res.(*GeocoderRet)
+	if !ok {
+		err = errors.New("api return nil pointer.")
+		return
+	}
+
+	if result.Status != 0 {
+		err = errors.New("baidu lbs cloud response error, status not zero.")
+		return
+	}
+
+	return
+}
+
 // Geocoding API包括地址解析和逆地址解析功能：
 // 地理编码：即地址解析，由详细到街道的结构化地址得到百度经纬度信息
 // 逆地理编码：即逆地址解析，由百度经纬度信息得到结构化地址信息
 // http://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-geocoding
-func (api BaiduApi) Geocoding (params map[string]string) (interface{}, error){
+func (api *BaiduLbsApi) Geocoding(params map[string]string) (interface{}, error) {
 	ret := interface{}(nil)
 	if _, ok := params["address"]; ok {
 		ret = &GeoEncoderRet{}
@@ -78,45 +165,45 @@ func (api BaiduApi) Geocoding (params map[string]string) (interface{}, error){
 		ret = &GeocoderRet{}
 	}
 	params["output"] = "json"
-	err := api.call("geocoder", "", params, ret)
+	err := api.call("geocoder", "", "v2", params, ret)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
+
 // 用于返回查询某个区域的某类POI数据，且提供单个POI的详情查询服务
 // http://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-placeapi
-func (api *BaiduApi) PlaceSearch (params map[string]string) (interface{}, error){
+func (api *BaiduLbsApi) PlaceSearch(params map[string]string) (interface{}, error) {
 	ret := &PlaceSearchRet{}
 	params["output"] = "json"
-	err := api.call("place", "search", params, ret)
+	err := api.call("place", "search", "v2", params, ret)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
+
 // 用于返回查询某个区域的某类POI数据，且提供单个POI的详情查询服务
 // http://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-placeapi
-func (api *BaiduApi) PlaceDetail (params map[string]string) (interface{}, error){
+func (api *BaiduLbsApi) PlaceDetail(params map[string]string) (interface{}, error) {
 	ret := &PlaceDetailRet{}
 	params["output"] = "json"
-	err := api.call("place", "detail", params, ret)
+	err := api.call("place", "detail", "v2", params, ret)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
+
 // Place suggestion API 是一套提供匹配用户输入关键字辅助信息、提示服务的接口
 // ，可返回json或xml格式的一组建议词条的数据。
-func (api *BaiduApi) Suggestion (params map[string]string) (interface{}, error){
+func (api *BaiduLbsApi) Suggestion(params map[string]string) (interface{}, error) {
 	ret := &SuggestionRet{}
 	params["output"] = "json"
-	err := api.call("place", "suggestion", params, ret)
+	err := api.call("place", "suggestion", "v2", params, ret)
 	if err != nil {
 		return nil, err
 	}
 	return ret, nil
 }
-
-
-
--- vo.go
+++ vo.go
@@ -7,16 +7,15 @@ type Point struct {
 	Y float64 `json:"y"`
 }
 
-func (point *Point) ToString() (string) {
+func (point *Point) ToString() string {
 	return strconv.FormatFloat(point.X, 'g', 10, 32) + "," +
-	strconv.FormatFloat(point.Y, 'g', 10,32) 
+		strconv.FormatFloat(point.Y, 'g', 10, 32)
 }
 
 type GeoconvRet struct {
-	Status int `json:"status"`
+	Status int     `json:"status"`
 	Result []Point `json:"result"`
 }
-
 
 type Location struct {
 	Lng float64 `json:"lng"`
@@ -24,126 +23,125 @@ type Location struct {
 }
 
 type AddressComponent struct {
-	Country string `json:"country"`
-	CountryCode int `json:"country_code"`
-	Province string `json:"province"`
-	City string `json:"city"`
-	District string `json:"district"`
-	Adcode string `json:"adcode"`
-	Street string `json:"street"`
+	Country       string `json:"country"`
+	CountryCode   int    `json:"country_code"`
+	Province      string `json:"province"`
+	City          string `json:"city"`
+	District      string `json:"district"`
+	Adcode        string `json:"adcode"`
+	Street        string `json:"street"`
 	Street_number string `json:"street_number"`
-	Direction string `json:"direction"`
-	Distance string `json:"distance"`
+	Direction     string `json:"direction"`
+	Distance      string `json:"distance"`
 }
 
 type Pois struct {
-	Addr string `json:"addr"`
-	Cp string `json:"cp"`
+	Addr      string `json:"addr"`
+	Cp        string `json:"cp"`
 	Direction string `json:"direction"`
-	Name string `json:"name"`
-	PoiType string `json:"poiType"`
-	Point Point `json:"point"`
-	Tag string `json:"tag"`
-	Tel string `json:"tel"`
-	Uid string `json:"uid"`
-	Zip string `json:"zip"`
+	Name      string `json:"name"`
+	PoiType   string `json:"poiType"`
+	Point     Point  `json:"point"`
+	Tag       string `json:"tag"`
+	Tel       string `json:"tel"`
+	Uid       string `json:"uid"`
+	Zip       string `json:"zip"`
 }
 
 type PoisRegions struct {
 	DirectionDesc string `json:"direction_desc"`
-	Name string `json:"name"`
-	Tag string `json:"tag"`
+	Name          string `json:"name"`
+	Tag           string `json:"tag"`
 }
 
 type GeocoderResult struct {
-	Location Location `json:"location"`
-	FormattedAddress string `json:"formatted_address"`
-	Business string `json:"business"`
-	AddressComponent AddressComponent `json:"addressComponent"`
-	Pois []Pois `json:"pois"`
-	PoisRegions []PoisRegions `json:"poisRegions"`
-	SematicDescription string `json:"sematic_description"`
-	CityCode int `json:"cityCode"`
+	Location           Location         `json:"location"`
+	FormattedAddress   string           `json:"formatted_address"`
+	Business           string           `json:"business"`
+	AddressComponent   AddressComponent `json:"addressComponent"`
+	Pois               []Pois           `json:"pois"`
+	PoisRegions        []PoisRegions    `json:"poisRegions"`
+	SematicDescription string           `json:"sematic_description"`
+	CityCode           int              `json:"cityCode"`
 }
 
 type GeoEncoderResult struct {
-	Location Location `json:"location"`
-	Precise int `json:"precise"`
-	Confidence int `json:"confidence"`
-	Level string `json:"level"`
+	Location   Location `json:"location"`
+	Precise    int      `json:"precise"`
+	Confidence int      `json:"confidence"`
+	Level      string   `json:"level"`
 }
 
 type GeocoderRet struct {
-	Status int `json:"status"`
+	Status int            `json:"status"`
 	Result GeocoderResult `json:"result"`
 }
 
 type GeoEncoderRet struct {
-	Status int `json:"status"`
+	Status int              `json:"status"`
 	Result GeoEncoderResult `json:"result"`
 }
 
 type DetailInfo struct {
-	Tag string `json:"tag"`
-	DetailUrl string `json:"detail_url"`
-	Type string `json:"type"`
-	Price string `json:"price"`
-	OverallRating string `json:"overall_rating"`
-	TasteRating string `json:"taste_rating"`
-	ServiceRating string `json:"service_rating"`
+	Tag              string `json:"tag"`
+	DetailUrl        string `json:"detail_url"`
+	Type             string `json:"type"`
+	Price            string `json:"price"`
+	OverallRating    string `json:"overall_rating"`
+	TasteRating      string `json:"taste_rating"`
+	ServiceRating    string `json:"service_rating"`
 	EnvionmentRating string `json:"environment_rating"`
-	ImageNum string `json:"image_num"`
-	CommentNum string `json:"comment_num"`
-	ShopHours string `json:"shop_hours"`
-	Atmosphere string `json:"atmosphere"`
-	FeaturedService string `json:"featured_service"`
-	Recommendation string `json:"recommendation"`
-	Description string `json:"description"`
-	Distance string `json:"distance"`
-	FacilityRating  string `json:"facility_rating"`
-	HygieneRating string `json:"hygiene_rating"`
+	ImageNum         string `json:"image_num"`
+	CommentNum       string `json:"comment_num"`
+	ShopHours        string `json:"shop_hours"`
+	Atmosphere       string `json:"atmosphere"`
+	FeaturedService  string `json:"featured_service"`
+	Recommendation   string `json:"recommendation"`
+	Description      string `json:"description"`
+	Distance         string `json:"distance"`
+	FacilityRating   string `json:"facility_rating"`
+	HygieneRating    string `json:"hygiene_rating"`
 	TechnologyRating string `json:"technology_rating"`
-	GrouponNum string `json:"groupon_num"`
-	DiscountNum string `json:"discount_num"`
-	FavoriteNum string `json:"favorite_num"`
-	CheckinNum string `json:"checkin_num"`
-
+	GrouponNum       string `json:"groupon_num"`
+	DiscountNum      string `json:"discount_num"`
+	FavoriteNum      string `json:"favorite_num"`
+	CheckinNum       string `json:"checkin_num"`
 }
 
 type PlaceResult struct {
-	Name string `json:"name"`
-	Location Location `json:"location"`
-	Address string `json:"address"`
-	Telephone string `json:"telephone"`
-	Detail int `json:"detail"`
-	Uid string `json:"uid"`
+	Name       string     `json:"name"`
+	Location   Location   `json:"location"`
+	Address    string     `json:"address"`
+	Telephone  string     `json:"telephone"`
+	Detail     int        `json:"detail"`
+	Uid        string     `json:"uid"`
 	DetailInfo DetailInfo `json:"detail_info"`
-	StreetId string `json:"street_id"`
+	StreetId   string     `json:"street_id"`
 }
 
 type PlaceSearchRet struct {
-	Status int `json:"status"`
-	Message string `json:"message"`
-	Total int `json:"total"`
-	Result []PlaceResult `json:"results"`
+	Status  int           `json:"status"`
+	Message string        `json:"message"`
+	Total   int           `json:"total"`
+	Result  []PlaceResult `json:"results"`
 }
 
 type PlaceDetailRet struct {
-	Status int `json:"status"`
-	Message string `json:"message"`
-	Result PlaceResult `json:"result"`
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Result  PlaceResult `json:"result"`
 }
 
 type SuggestionResult struct {
-	name string `json:"name"`
+	name     string   `json:"name"`
 	Location Location `json:"location"`
-	Uid string `json:"uid"`
-	City string `json:"city"`
-	District string `json:"district"`
+	Uid      string   `json:"uid"`
+	City     string   `json:"city"`
+	District string   `json:"district"`
 }
 
 type SuggestionRet struct {
-	Status int `json:"status"`
-	Message string `json:"message"`
-	Result []SuggestionResult `json:"result"`
+	Status  int                `json:"status"`
+	Message string             `json:"message"`
+	Result  []SuggestionResult `json:"result"`
 }
